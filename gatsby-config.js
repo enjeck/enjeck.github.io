39fd0d49a3f9c177dcdaa64889ae8876dadd7f1b
Enjeck <patrathewhiz@gmail.com>
2021-11-22 16:18:22 +0100

    Enable Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,31 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-W8GSQN5XSZ", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        //gtagConfig: {
+          //optimize_id: "OPT_CONTAINER_ID",
+          //anonymize_ip: true,
+          //cookie_expires: 0,
+        //},
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
