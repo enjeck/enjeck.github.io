e7bd03b160e6854c858c0be88d3e45be2d245ddd
Enjeck <patrathewhiz@gmail.com>
2021-11-22 15:52:48 +0100

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,12 +56,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
