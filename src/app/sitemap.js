51bd6fd8360be7c89bb0720118f6d1f291e21d5c
Enjeck C. <patrathewhiz@gmail.com>
2025-12-14 15:52:21 +0000

    Force static generation for robots.txt and sitemap.xml routes.

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,10 @@
 import { getAllPosts } from '@/lib/blog'
 
+export const dynamic = 'force-static'
+
 export default function sitemap() {
   const baseUrl = 'https://enjeck.github.io'
-  
+
   const posts = getAllPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: new Date(post.frontmatter.date),
